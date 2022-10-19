5c4810ec2d64fb15baa4806a3497c64a73a77d18
Peeters <Peja090604@leerling.mosa-rt.be>
2022-10-19 08:53:09 +0200

    oqisdoqsijdi

--- hfst_2/oefen_mee/oefenmee9_Deel2.py
+++ hfst_2/oefen_mee/oefenmee9_Deel2.py
@@ -1,6 +1,17 @@
-import _json
 import json
 
 fp = open("Leerkracht_Bestanden/6IICT_PROG4_oef/hfst_2/oefen_mee/oefenmee9.json", "r")
 quiz = json.load(fp)
 fp.close()
+
+print(quiz)
+
+for key, onderwerp in quiz["quiz"].items():
+    for key2, vraag in onderwerp.items():
+       print(vraag["vraag"])
+       print(vraag["opties"])
+       beantwoord = input("Wat is het juiste antwoord?")
+       if beantwoord == vraag["antwoord"]:
+        print("Dit is juist")
+       else:
+        print("Fout")
