994a682d54e748e6590f3c273bdbd31962f3ac19
Peeters <Peja090604@leerling.mosa-rt.be>
2022-11-28 11:19:41 +0100

    update

--- hfst_3/oefeningen/oefen_mee5.py
+++ hfst_3/oefeningen/oefen_mee5.py
@@ -2,20 +2,28 @@
 Wat gaat hier mis?
 """
 fruit_lijst = ["Appel", "Banaan", "Meloen", "Mango", "Druif"]
-getal = int( input("Hoeveel fruit uit de lijst wil je printen: ") )
-
-for i in range(getal):
-    fruit = fruit_lijst[i]
-    print(fruit)
+try:
+    getal = int( input("Hoeveel fruit uit de lijst wil je printen: ") )
+    for i in range(getal):
+        fruit = fruit_lijst[i]
+        print(fruit)
+except IndexError:
+    print("De opgegeven waarde is te groot.")
+except ZeroDivisionError:
+    print(f"Het getal 0 kan niet worden opgegeven.")
+except ValueError:
+    print(f"De ingegeven waarden is geen getal.")
+except:
+    print("onbekende fout opgetreden.")
 
 
 """ Niveau 3 (haal uit commentaar) """
-# while True:
-#     fruit = input("Element aan lijst toevoegen: ")
+while True:
+    fruit = input("Element aan lijst toevoegen: ")
     
-#     if fruit == "":
-#         break # Loop stopt wanneer gebruiker een lege string ingeeft.
-#     else:
-#         fruit_lijst.append(fruit)
+    if fruit == "":
+        break # Loop stopt wanneer gebruiker een lege string ingeeft.
+    else:
+        fruit_lijst.append(fruit)
 
-# print(fruit_lijst)
+print(fruit_lijst)
